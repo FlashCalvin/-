33a528d6bc50c427a5239a17df33d33783b2da61
user <user@es301n30>
2019-02-26 12:03:22 +0800

    asshole

---
+++ Hello world/src/com/tom/GraduateStudent.java
@@ -0,0 +1,8 @@
+package com.tom;
+
+public class GraduateStudent extends Student {
+	public GraduateStudent() {
+		super("no one",0);
+	}
+
+}
---
+++ Hello world/src/com/tom/Student.java
@@ -0,0 +1,16 @@
+package com.tom;
+
+public class Student {
+		protected String name;
+		private int English;
+		public Student(String name , int English) {
+		this.name = name;
+		this.English = English;
+	}
+		public void print() {
+			System.out.println(name + "\t" + English );
+		}
+//		public void
+			
+
+}
