91ee01b386c28a5a260b4a88e54a166a3637e689
user <user@es301n30>
2019-03-26 12:10:45 +0800

    ass

---
+++ Hello world/src/com/tom/lala.java
@@ -0,0 +1,14 @@
+package com.tom;
+
+import java.util.Scanner;
+
+public class lala {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("請輸入品項");
+
+	}
+
+}
