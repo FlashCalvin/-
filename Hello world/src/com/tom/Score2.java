3ddaf186133a8e014914ab7b307da311c3974756
user <user@es301n40>
2019-04-30 12:14:35 +0800

    hfghj

--- Hello world/src/com/tom/Score2.java
+++ Hello world/src/com/tom/Score2.java
@@ -1,13 +1,14 @@
-//package com.tom;
-//
-//public class Score2 {
-//	int value;
-//
-//	   Score(String s) throws ScoreFormatException{
-//	   try{
-//		   value = Integer.parseInt(s);
-//	   }catch(NumberFormatException e) {
-//		    throw new ScoreFormatException();
-//	   }
-//	   }
-//	    }
+package com.tom;
+
+public class Score2 {
+	int value;
+
+	   public Score2(String s) throws ScoreFormatException{
+	   
+	   try{
+		   value = Integer.parseInt(s);
+	   }catch(NumberFormatException e) {
+		    throw new ScoreFormatException();
+	   }
+	   }
+}
