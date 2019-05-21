9b4b751c873bf42a1c104c88c139049b1c36da07
user <user@es301n40>
2019-05-21 12:03:18 +0800

    fgdg

--- Hello world/src/com/tom/Sales.java
+++ Hello world/src/com/tom/Sales.java
@@ -1,9 +1,18 @@
 package com.tom;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class Sales {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		try {
+			FileInputStream k = new FileInputStream("Sales.txt");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
